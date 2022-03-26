aea6ddde6c01c87a97a3fe03ea7c794f612645dd
jimous <jimouspeng@xiaoe-tech.com>
2022-03-26 21:50:59 +0800

    fix: node-stream

--- process.js
+++ process.js
@@ -2,9 +2,9 @@ const process = require('process')
 
 process.exitCode = 666
 
-console.log(process.versions, 'node--version');
+console.log(process.versions, 'node--version')
 
-console.log(process.execArgv, '看看process.execArgv', process.pid, process.release);
+console.log(process.execArgv, '看看process.execArgv', process.pid, process.release)
 
 process.on('exit', (code) => {
     console.log('进程执行exit', code)
@@ -12,11 +12,16 @@ process.on('exit', (code) => {
 
 let i = 0
 
-setInterval(() => {
-    i++
-    console.log(i)
-    if (i > 5) {
-        process.exit()
-        // process.abort()
-    }
-}, 1000)
+// setInterval(() => {
+//     i++
+//     console.log(i)
+//     if (i > 5) {
+//         process.exit()
+//         // process.abort()
+//     }
+// }, 1000)
+
+process.stdin.on('data', (data) => {
+    console.log(`You typed ${data.toString()}`)
+    process.exit()
+})
--- stream.js
+++ stream.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const stream = require('stream')
 // const file = fs.createWriteStream('./big.file')
 // for (let i = 0; i <= 1e6; i++) {
 //     file.write('jimous is cool !!! \n')
@@ -7,10 +8,25 @@ const fs = require('fs')
 
 // 打开一个可读流
 const file = fs.createReadStream('./test.txt', 'utf-8')
-file.on('data', (chunk) => {
-    console.log('data:', chunk)
+file.on('readable', (chunk) => {
+    console.log('data:', chunk, file.read())
 })
+
+console.log(file.destroyed, file.isPaused(), file.readable, file.readableLength);
 
 file.on('end', function () {
     console.log('END')
 })
+
+file.on('close', function () {
+    console.log('close')
+    file.destroy()
+})
+
+file.on('error', function (err) {
+    console.log('error:', err)
+})
+
+// file.destroy('关闭流')
+
+console.log(file.destroyed);
