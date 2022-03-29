72d9a881bd49ea606908f7fa8dbe6b080bc9a609
jimous <jimouspeng@xiaoe-tech.com>
2022-03-30 00:47:01 +0800

    buf

--- buffer.js
+++ buffer.js
@@ -4,12 +4,36 @@
  * @Description: buffer操作
  * @FilePath: \node\buffer.js
  */
-const { Buffer } = require('buffer');
+const { Buffer } = require('buffer')
 
-const oneBuf = Buffer.from('jimous', 'utf8');
+const oneBuf = Buffer.from(new String('this is a test'))
 
-const arrayBuf = Buffer.from([1, 2, 3]);
+const arrayBuf = Buffer.from([1, 2, 3])
 
-console.log(oneBuf.toString());
+// const isBuf = oneBuf[1]
 
-console.log(new Uint32Array(arrayBuf));
+// console.log('isBuf[1]: ', isBuf.toString('utf8'))
+
+const str = 'Node.js'
+const buf = Buffer.allocUnsafe(str.length)
+
+for (let i = 0; i < str.length; i++) {
+    buf[i] = str.charCodeAt(i)
+}
+
+console.log(buf.toString('utf8'), '看看')
+
+console.log(oneBuf[1], oneBuf.toString(), Buffer.compare(oneBuf, arrayBuf))
+
+console.log(new Uint32Array(arrayBuf))
+
+const strBuf = Buffer.allocUnsafe(0)
+console.log(strBuf, strBuf.toString('utf-8'))
+
+console.log(Buffer.byteLength('sfefse'))
+
+// console.log('单个 Buffer 实例允许的最大大小:', strBuf.constants.MAX_LENGTH);
+
+const oneBlob = new strBuf.Blob('jimous is cool')
+
+console.log(oneBlob)
