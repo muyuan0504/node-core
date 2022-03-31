33e8e29f7c31d992713228724bc6d2e9a5034d4c
jimous <jimouspeng@xiaoe-tech.com>
2022-03-31 23:24:04 +0800

    父子进程通信新方式

--- childSrc/child_a.js
+++ childSrc/child_a.js
@@ -1,2 +1,6 @@
-console.log('运行了child_a');
-process.exit(999);
+console.log('运行了child_a')
+// process.exit(999);
+process.stdin.on('data', (chunk) => {
+    console.log(chunk.toString(), '收到父进程`````````````````````````````````````````')
+    process.stdout.write('尝试给父进程发消息')
+})
--- child_process.js
+++ child_process.js
@@ -1,42 +1,46 @@
 /*
  * @Date: 2022-03-30 23:05:08
- * @LastEditors: Please set LastEditors
+ * @LastEditors: jimouspeng
  * @Description: nodejs-子进程
- * @LastEditTime: 2022-03-31 13:30:53
+ * @LastEditTime: 2022-03-31 23:23:28
  * @FilePath: \node\child_process.js
  */
 
-const path = require('path');
-const execCwd = path.resolve('./childSrc');
+const path = require('path')
+const execCwd = path.resolve('./childSrc')
 
 /** 异步创建子进程方式 */
-const { spawn, exec, execFile, fork } = require('child_process');
+const { spawn, exec, execFile, fork } = require('child_process')
 
 const syncChildA = spawn('node', ['child_a.js'], {
     cwd: execCwd,
-});
+})
+
+/** 与主进程的stdin是可读流不同，创建的子进程的stdin是可写流 */
+syncChildA.stdin.write('父进程给childA发送消息')
+
 syncChildA.stdout.on('data', (chunk) => {
-    console.log(chunk.toString());
-});
+    console.log(chunk.toString(), '打印了输入内容???')
+})
 syncChildA.on('close', (code) => {
-    console.log('子进程A已退出，退出码 ' + code);
-});
+    console.log('子进程A已退出，退出码 ' + code)
+})
 
 const syncChildB = exec(
     'node child_b.js',
     {
         cwd: execCwd,
     },
-    (err, data) => {
-        // console.log(err, data);
+    (err, stdout, stderr) => {
+        console.log(err, stdout, stderr, '打印看看child_b.js回调')
     }
-);
+)
 syncChildB.stdout.on('data', (chunk) => {
-    console.log(chunk.toString());
-});
+    console.log(chunk.toString())
+})
 syncChildB.on('close', (code) => {
-    console.log('子进程B已退出，退出码 ' + code);
-});
+    console.log('子进程B已退出，退出码 ' + code)
+})
 
 const syncChildC = execFile(
     'node',
@@ -47,43 +51,43 @@ const syncChildC = execFile(
     (err, data) => {
         // console.log(err, data);
     }
-);
+)
 syncChildC.stdout.on('data', (chunk) => {
-    console.log(chunk.toString());
-});
+    console.log(chunk.toString())
+})
 syncChildC.on('close', (code) => {
-    console.log('子进程c已退出，退出码 ' + code);
-});
+    console.log('子进程c已退出，退出码 ' + code)
+})
 syncChildC.on('message', (msg) => {
-    console.log(msg, '收到消息没有'); // 由于execFile创建的子进程不会产生内置通信通道，所以无法直接通信
-});
+    console.log(msg, '收到消息没有') // 由于execFile创建的子进程不会产生内置通信通道，所以无法直接通信
+})
 
 const syncChildD = fork('./child_d.js', ['child'], {
     cwd: execCwd,
-});
+})
 
 // syncChildD.disconnect();
-syncChildD.send('直接这里发个消息哈哈哈');
+syncChildD.send('直接这里发个消息哈哈哈')
 syncChildD.on('message', (msg) => {
-    console.log(msg, '收到消息', syncChildD.pid);
-});
+    console.log(msg, '收到消息', syncChildD.pid)
+})
 syncChildD.on('disconnect', () => {
-    console.log('disconnect-----------');
-});
+    console.log('disconnect-----------')
+})
 syncChildD.on('close', (code, sign) => {
-    console.log('子进程-D退出-------', code, sign);
-});
+    console.log('子进程-D退出-------', code, sign)
+})
 setTimeout(() => {
-    syncChildD.kill();
-}, 500);
+    syncChildD.kill()
+}, 500)
 
 setTimeout(() => {
-    console.log('起一个循环吧');
-}, 5000);
+    console.log('起一个循环吧')
+}, 5000)
 
 process.stdin.on('data', (err, data) => {
-    console.log(err, data, '收集输入');
-});
+    console.log(err, data, '收集输入')
+})
 
 /** 同步创建子进程方式 */
 // const { spawnSync, execSync, execFileSync } = require('child_process');
