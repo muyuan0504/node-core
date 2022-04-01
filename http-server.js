c6e474051a08dc214e6bf95b1083e7ca1eddd106
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-04-01 11:57:15 +0800

    http-server

--- http-server.js
+++ http-server.js
@@ -1,7 +1,7 @@
 const { createServer } = require('http');
 
 const server = createServer((req, res) => {
-    console.log();
+    console.log(req.writable, res.writable); // undefined, true
     res.writeHead(200, { 'content-Type': 'application/json', 'sleft-content': 'jimous-server' });
     res.end('jimous server' + JSON.stringify(req.headers));
 });
