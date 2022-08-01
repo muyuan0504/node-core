8bd05b067c73bec604fcc4d60ef4aa2b05b75dd9
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-08-01 18:06:00 +0800

    fix: http强缓存&协商缓存本地实践

--- HTTP/http.js
+++ HTTP/http.js
@@ -1,4 +1,7 @@
+/** 强缓存 & 协商缓存 本地测试 */
 const http = require('http');
+
+let init = false;
 
 const httpCtx = http.createServer((req, res) => {
     if (req.url.indexOf('/favicon.ico') > -1) {
@@ -6,15 +9,27 @@ const httpCtx = http.createServer((req, res) => {
         return;
     }
     console.log(req.headers);
+    if (new Date().getTime() - Number(req.headers['if-modified-since']) < 8 * 1000) {
+        init = true;
+        console.log('进缓存了');
+        res.writeHead(304, {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Last-Modified': new Date().getTime(),
+        });
+        res.end('111');
+        return;
+    }
     // res.setHeader('Expires', new Date(Date.now() + 20000).toGMTString());
     // res.setHeader('Cache-Control', 'max-age=60');
     res.writeHead(200, {
-        'content-Type': 'application/json',
-        Expires: new Date('2022-07-19').toGMTString(),
+        'content-Type': 'application/json;charset=utf-8',
+        // Expires: new Date('2022-08-19').toGMTString(),
         // 'Cache-Control': 'max-age=60000',
-        // 'Last-Modified': 60 * 2,
+        // Etag: '1212',
+        'Last-Modified': new Date().getTime(),
     });
-    res.end('jimous is cool 2132323');
+    const data = init ? '缓存刷新-----' : 'jimous is cool 2132323';
+    res.end(data);
 });
 
 httpCtx.listen(8000, () => {
